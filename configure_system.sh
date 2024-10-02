31dc51fda46ebfb9f3d40d818b3b4f563cd4d717
O-Danylchuk <odanylchuk@kse.org.ua>
2024-10-02 15:17:49 +0300

    Code part is done

--- configure_system.sh
+++ configure_system.sh
@@ -1,38 +1,38 @@
-packets=("tcptracerout" "nethogs")
+#!/bin/bash
 
-for packet in ${packets[@]}; do
-        if dpkg -l | grep -q $packet; then
-		echo "Package $packet already installed"
-	else
-		sudo apt-get install $packet -y	
-       		if dpkg -l | grep -q $packet; then
-	       		echo "Package $packet installed successfully"
-       		fi
-	fi
+packages=("tcptraceroute" "nethogs")
+
+for packet in "${packages[@]}"; do
+    if dpkg -l | grep -q "$packet"; then
+        echo "Package $packet already installed"
+    else
+        sudo apt-get install "$packet" -y
+        if dpkg -l | grep -q "$packet"; then
+            echo "Package $packet installed successfully"
+        fi
+    fi
 done
 
-
-if test -d /usr/bin/believer; then
-	touch /usr/bin/believer/config.pl 
+if [ -d /usr/bin/believer ]; then
+    touch /usr/bin/believer/config.pl
 else
-	echo "Directory does not exist. Creating..."
-	sudo mkdir -p /usr/bin/believer/src
-	sudo touch /usr/bin/believer/src/config.pl
+    echo "Directory does not exist. Creating..."
+    sudo mkdir -p /usr/bin/believer/src
+    sudo touch /usr/bin/believer/src/config.pl
 fi
 
-
-if test -e simpleService.sh
-	sudo chmod +x simpleService.sh
-	mv simpleService.sh /etc/
+if [ -e simpleService.sh ]; then
+    sudo chmod +x simpleService.sh
+    sudo mv -f simpleService.sh /etc/
 else
-	sudo touch /etc/simpleService.sh
-	sudo chmod +x /etc/simpleService.sh
+    sudo touch /etc/simpleService.sh
+    sudo chmod +x /etc/simpleService.sh
 fi
 
-
-if test -e simpleService.service
-	sudo mv simpleService.service /etc/systemd/system/simpleService.service
-else 
-	echo "file simpleService.service not found. Exiting..."
-	exit(1)
+if [ -e simpleService.service ]; then
+    sudo mv -f simpleService.service /etc/systemd/system/simpleService.service
+else
+    echo "File simpleService.service not found. Exiting..."
+    exit 1
 fi
+
